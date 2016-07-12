cd078e57b44d6e057600e72524b9ca9249c79327
changzheng <sx_changzheng@163.com>
2016-07-12 18:05:28 +0800

    增加用户认证，同步登录的功能。

--- core/controllers/uc.py
+++ core/controllers/uc.py
@@ -5,9 +5,14 @@ from core.controllers import base
 from core.platform import models
 from core.controllers import ucnote
 
+API_RETURN_SUCCEED = '1'
+API_RETURN_FAILED = '-1'
+API_RETURN_FORBIDDEN = '1'
+API_SYNLOGIN = 1
 
 current_user_services = models.Registry.import_current_user_services()
 (user_models,) = models.Registry.import_models([models.NAMES.user])
+
 
 class UcApiHandler(base.BaseHandler):
     def get(self):
@@ -15,6 +20,8 @@ class UcApiHandler(base.BaseHandler):
         code = ucnote.uc_authcode(code, 'DECODE')
         get = urlparse.parse_qs(code)
         action = get['action'][0]
+        post = self.request.body
+
         if action in ('test', 'deleteuser', 'renameuser', 'gettag',
                       'synlogin', 'synlogout', 'updatepw',
                       'updatebadwords', 'updatehosts',
@@ -22,8 +29,20 @@ class UcApiHandler(base.BaseHandler):
                       'updatecredit', 'getcredit',
                       'getcreditsettings', 'updatecreditsettings',
                       'addfeed'):
-            self.response.out.write(1)
+            result = getattr(self, action)(get, post)
+            self.response.out.write(result)
         else:
             self.response.out.write(-1)
 
+    def test(self, get, post):
+        get = get + post
+        return API_RETURN_SUCCEED
+    def deleteuser(self, get, post):
+        get = get + post
+        return API_RETURN_SUCCEED
 
+    def synlogout(self, get, post):
+        get = get + post
+        if not API_SYNLOGIN:
+            return API_RETURN_FORBIDDEN
+        return API_RETURN_SUCCEED
