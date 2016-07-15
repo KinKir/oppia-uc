0f4d544e9b92e958b2239b1ffdaae64f38f6bb79
changzheng <sx_changzheng@163.com>
2016-07-15 18:10:35 +0800

    Merge branch 'develop' of https://github.com/zgchizi/oppia-uc into develop
    # Conflicts:
    #	core/controllers/uc.py

--- core/controllers/uc.py
+++ core/controllers/uc.py
@@ -1,13 +1,21 @@
+import Cookie
+import hashlib
+import logging
 import urlparse
 import xml.etree.cElementTree as ET
 from core.controllers import base
 from core.platform import models
 from core.controllers import ucnote
 
+# from core.domain import user_services
+
 API_RETURN_SUCCEED = '1'
 API_RETURN_FAILED = '-1'
 API_RETURN_FORBIDDEN = '1'
 API_SYNLOGIN = 1
+
+# Name of the cookie that stores the user info.
+_COOKIE_NAME = 'dev_appserver_login'
 
 current_user_services = models.Registry.import_current_user_services()
 (user_models,) = models.Registry.import_models([models.NAMES.user])
@@ -20,7 +28,8 @@ class UcApiHandler(base.BaseHandler):
         get = dict(urlparse.parse_qsl(code))
         action = get['action']
         post = self.request.body
-        post = self.xml_unserilizae(post)
+        post = self.xml_unserilize(post)
+        logging.info("call action %s", action)
         if action in ('test', 'deleteuser', 'renameuser', 'gettag',
                       'synlogin', 'synlogout', 'updatepw',
                       'updatebadwords', 'updatehosts',
@@ -33,13 +42,14 @@ class UcApiHandler(base.BaseHandler):
         else:
             self.response.out.write(-1)
 
-    def xml_unserilizae(self, xml):
-        xmlnode = ET.fromstring(xml)
-        root = xmlnode.getroot()
+    def xml_unserilize(self, xml):
+        if not xml:
+            return {}
+        root = ET.fromstring(xml)
         data = {}
         for child in root:
             data[child.attrib['id']] = child.text
-            if subchild in child:
+            if len(child) or (child is not None):
                 childdata = {}
                 for subchild in child:
                     childdata[subchild.attrib['id']] = subchild.text
@@ -47,7 +57,7 @@ class UcApiHandler(base.BaseHandler):
         return data
 
     def test(self, get, post):
-        get = get + post
+        get = get.update(post)
         return API_RETURN_SUCCEED
 
     def deleteuser(self, get, post):
@@ -63,11 +73,14 @@ class UcApiHandler(base.BaseHandler):
         return API_RETURN_SUCCEED
 
     def synlogin(self, get, post):
-        get = get + post
+        """uid = get['uid']"""
+        username = get['username'] + post
+        self.response.headers['Set-Cookie'] = \
+            _set_user_info_cookie(username, False)
         return API_RETURN_SUCCEED
 
     def synlogout(self, get, post):
-        get = get + post
+        get = get.update(post)
         if not API_SYNLOGIN:
             return API_RETURN_FORBIDDEN
         return API_RETURN_SUCCEED
@@ -91,7 +104,8 @@ class UcApiHandler(base.BaseHandler):
         return API_RETURN_SUCCEED
 
     def updateapps(self, get, post):
-        get = get + post
+        # get = get + post
+        logging.info("%s%s", get, post)
         if not API_SYNLOGIN:
             return API_RETURN_FORBIDDEN
         return API_RETURN_SUCCEED
@@ -131,3 +145,84 @@ class UcApiHandler(base.BaseHandler):
         if not API_SYNLOGIN:
             return API_RETURN_FORBIDDEN
         return API_RETURN_SUCCEED
+
+
+def get_user_info(http_cookie, cookie_name=_COOKIE_NAME):
+    """Gets the requestor's user info from an HTTP Cookie header.
+
+    Args:
+      http_cookie: The value of the 'Cookie' HTTP request header.
+      cookie_name: The name of the cookie that stores the user info.
+
+    Returns:
+      A tuple (email, admin, user_id) where:
+        email: The user's email address, if any.
+        admin: True if the user is an admin; False otherwise.
+        user_id: The user ID, if any.
+    """
+    try:
+        cookie = Cookie.SimpleCookie(http_cookie)
+    except Cookie.CookieError:
+        return '', False, ''
+
+    cookie_dict = dict((k, v.value) for k, v in cookie.iteritems())
+    return _get_user_info_from_dict(cookie_dict, cookie_name)
+
+
+def _get_user_info_from_dict(cookie_dict, cookie_name=_COOKIE_NAME):
+    """Gets the requestor's user info from a cookie dictionary.
+
+    Args:
+      cookie_dict: A dictionary mapping cookie names onto values.
+      cookie_name: The name of the cookie that stores the user info.
+
+    Returns:
+      A tuple (email, admin, user_id) where:
+        email: The user's email address, if any.
+        admin: True if the user is an admin; False otherwise.
+        user_id: The user ID, if any.
+    """
+    cookie_value = cookie_dict.get(cookie_name, '')
+
+    email, admin, user_id = (cookie_value.split(':') + ['', '', ''])[:3]
+    if '@' not in email:
+        if email:
+            logging.warning('Ignoring invalid login cookie: %s', cookie_value)
+        return '', False, ''
+    return email, (admin == 'True'), user_id
+
+
+def _create_cookie_data(email, admin):
+    """Creates cookie payload data.
+
+    Args:
+      email: The user's email address.
+      admin: True if the user is an admin; False otherwise.
+
+    Returns:
+      A string containing the cookie payload.
+    """
+    if email:
+        user_id_digest = hashlib.md5(email.lower()).digest()
+        user_id = '1' + ''.join(['%02d' % ord(x) for x in user_id_digest])[:20]
+    else:
+        user_id = ''
+    return '%s:%s:%s' % (email, admin, user_id)
+
+
+def _set_user_info_cookie(email, admin, cookie_name=_COOKIE_NAME):
+    """Creates a cookie to set the user information for the requestor.
+
+    Args:
+      email: The email to set for the user.
+      admin: True if the user should be admin; False otherwise.
+      cookie_name: The name of the cookie that stores the user info.
+
+    Returns:
+      Set-Cookie value for setting the user info of the requestor.
+    """
+    cookie_value = _create_cookie_data(email, admin)
+    cookie = Cookie.SimpleCookie()
+    cookie[cookie_name] = cookie_value
+    cookie[cookie_name]['path'] = '/'
+    return cookie[cookie_name].OutputString()
--- core/controllers/ucnote.py
+++ core/controllers/ucnote.py
@@ -1,9 +1,8 @@
-
 import math
 import time
 import urllib
 import urllib2
-
+import xml.etree.cElementTree as ET
 
 URL = 'http://localhost/uc_server/index.php'
 UC_CLIENT_RELEASE = '20141101'
@@ -11,110 +10,152 @@ UC_KEY = "4364Ofz0ebI4vak4V1t7D5j4udYfMfw4K7P2sbBbR4A270q2149093PdPdDavaj0"
 UC_APPID = "2"
 UC_IP = '127.0.0.1'
 
-def uc_user_login(username, password, isuid=0, checkques=0, \
+
+def uc_user_login(username, password, isuid=0, checkques=0,
                   questionid='', answer='', local_ip=''):
     return uc_api_post("user", "login",
-                       {'username':username, \
-                       'password':password, "isuid":isuid, \
-                       'checkques':checkques, \
-                       'questionid':questionid, \
-                       'answer':answer, 'ip':local_ip})
+                       {'username': username,
+                        'password': password, "isuid": isuid,
+                        'checkques': checkques,
+                        'questionid': questionid,
+                        'answer': answer, 'ip': local_ip})
+
+
+def xml_unserilize(xml):
+    if not xml:
+        return {}
+    root = ET.fromstring(xml)
+    data = {}
+    for child in root:
+        data[child.attrib['id']] = child.text
+        if len(child) or (child is not None):
+            childdata = {}
+            for subchild in child:
+                childdata[subchild.attrib['id']] = subchild.text
+            data[child.attrib['id']] = childdata
+    return data
+
 
 def uc_app_ls():
     returns = uc_api_post('app', 'ls', {})
     return returns
 
-def uc_feed_add(icon, uid, username, title_template='', \
+
+def uc_feed_add(icon, uid, username, title_template='',
                 title_data='', body_template='',
                 body_data='', body_general='', target_ids='', images=None):
     if images is None:
         images = []
 
-    return uc_api_post('feed', 'add', {'icon':icon, \
-                'appid':UC_APPID, 'uid':uid, \
-                'username':username, 'title_template':title_template, \
-                'title_data':title_data, 'body_template':body_template, \
-                'body_data':body_data, \
-                'body_general':body_general, 'target_ids':target_ids, \
-                'image_1':images[0]['url'], \
-                'image_1_link':images[0]['link'], \
-                'image_2':images[1]['url'], \
-                'image_2_link':images[1]['link'], \
-                'image_3':images[2]['url'], \
-                'image_3_link':images[2]['link'], \
-                'image_4':images[3]['url'], \
-                'image_4_link':images[3]['link']})
+    return uc_api_post('feed', 'add',
+                       {'icon': icon,
+                        'appid': UC_APPID, 'uid': uid,
+                        'username': username,
+                        'title_template': title_template,
+                        'title_data': title_data,
+                        'body_template': body_template,
+                        'body_data': body_data,
+                        'body_general': body_general,
+                        'target_ids': target_ids,
+                        'image_1': images[0]['url'],
+                        'image_1_link': images[0]['link'],
+                        'image_2': images[1]['url'],
+                        'image_2_link': images[1]['link'],
+                        'image_3': images[2]['url'],
+                        'image_3_link': images[2]['link'],
+                        'image_4': images[3]['url'],
+                        'image_4_link': images[3]['link']})
+
 
 def uc_feed_get(limit=100, delete=True):
-    returns = uc_api_post('feed', 'get', {'limit':limit, 'delete':delete})
+    returns = uc_api_post('feed', 'get', {'limit': limit, 'delete': delete})
     return returns
 
-def uc_user_register(username, password, email, questionid='', \
+
+def uc_user_register(username, password, email, questionid='',
                      answer='', regip=''):
     returns = uc_api_post('user', 'register',
-                          {'username':username, 'password':password, \
-                            'email':email, 'questionid':questionid, \
-                            'answer':answer, 'regip':regip})
+                          {'username': username, 'password': password,
+                           'email': email, 'questionid': questionid,
+                           'answer': answer, 'regip': regip})
     return returns
+
 
 def uc_user_synlogin(uid):
     uid = int(uid)
-    return uc_api_post('user', 'synlogin', {'uid':uid})
+    return uc_api_post('user', 'synlogin', {'uid': uid})
+
 
 def uc_user_synlogout():
     return uc_api_post('user', 'synlogout', [])
 
-def uc_user_edit(username, oldpw, newpw, email, \
-                ignoreoldpw=0, questionid='', answer=''):
-    return uc_api_post('user', 'edit', {'username':username, \
-                         'oldpw':oldpw, 'questionid':questionid, \
-                         'ignoreolepw':ignoreoldpw, \
-                         'newpw':newpw, 'email':email, \
-                         'answer':answer})
+
+def uc_user_edit(username, oldpw, newpw, email,
+                 ignoreoldpw=0, questionid='', answer=''):
+    return uc_api_post('user', 'edit', {'username': username,
+                                        'oldpw': oldpw,
+                                        'questionid': questionid,
+                                        'ignoreolepw': ignoreoldpw,
+                                        'newpw': newpw, 'email': email,
+                                        'answer': answer})
+
 
 def uc_user_delete(uid):
-    return uc_api_post('user', 'delete', {'uid':uid})
+    return uc_api_post('user', 'delete', {'uid': uid})
+
 
 def uc_user_deleteavatar(uid):
-    return uc_api_post('user', 'deleteavatar', {'uid':uid})
+    return uc_api_post('user', 'deleteavatar', {'uid': uid})
+
 
 def uc_user_checkname(username):
-    return uc_api_post('user', 'check_username', {'username':username})
+    return uc_api_post('user', 'check_username', {'username': username})
+
 
 def uc_user_checkemail(email):
-    return uc_api_post('user', 'check_email', {'email':email})
+    return uc_api_post('user', 'check_email', {'email': email})
+
 
 def uc_user_addprotected(username, admin=''):
     return uc_api_post('user', 'addprotected',
-                       {'username':username, 'admin':admin})
+                       {'username': username, 'admin': admin})
+
 
 def uc_user_deleteprotected(username):
-    return uc_api_post('user', 'deleteprotected', {'username':username})
+    return uc_api_post('user', 'deleteprotected', {'username': username})
+
 
 def uc_user_getprotected():
-    return uc_api_post('user', 'getprotected', {'1':1})
+    return uc_api_post('user', 'getprotected', {'1': 1})
+
 
 def uc_get_user(username, isuid=0):
     returns = uc_api_post('user', 'get_user',
-                          {'username':username, 'isuid':isuid})
+                          {'username': username, 'isuid': isuid})
     return returns
+
 
 def uc_user_merge(oldusername, newusername, uid, password, email):
-    returns = uc_api_post('user', 'merge', {'oldusername':oldusername, \
-                   'newusername':newusername, \
-                   'uid':uid, 'password':password, 'email':email})
+    returns = uc_api_post('user', 'merge',
+                          {'oldusername': oldusername,
+                           'newusername': newusername,
+                           'uid': uid, 'password': password, 'email': email})
     return returns
 
+
 def uc_user_merge_remove(username):
-    return uc_api_post('user', 'merge_remove', {'username':username})
+    return uc_api_post('user', 'merge_remove', {'username': username})
+
 
 def uc_user_getcredit(appid, uid, credit):
     return uc_api_post('user', 'getcredit',
-                       {'appid':appid, 'uid':uid, 'credit':credit})
+                       {'appid': appid, 'uid': uid, 'credit': credit})
+
 
 def uc_user_logincheck(username, local_ip):
-    return uc_api_post('user', 'logincheck', \
-                       {'username':username, 'ip':local_ip})
+    return uc_api_post('user', 'logincheck',
+                       {'username': username, 'ip': local_ip})
+
 
 def uc_api_post(module, action, arg=None):
     if arg is None:
@@ -136,9 +177,11 @@ def uc_api_post(module, action, arg=None):
     postdata = uc_api_requestdata(module, action, s)
     return uc_fopen2(URL, postdata)
 
+
 def uc_fopen2(url, post='', timeout=15):
     # todo add times parameter
     return uc_fopen(url, post, timeout)
+
 
 def uc_fopen(url, post='', timeout=15):
     req = urllib2.Request(url)
@@ -148,12 +191,13 @@ def uc_fopen(url, post='', timeout=15):
     response = opener.open(req, data=post, timeout=timeout)
     return response.read()
 
-def uc_api_requestdata(module, action, arg='', extra=''):
 
+def uc_api_requestdata(module, action, arg='', extra=''):
     post = "m=" + module + "&a=" + action + "&inajax=2&release=" \
            + UC_CLIENT_RELEASE
     post += "&input=" + uc_api_input(arg) + "&appid=" + UC_APPID + extra
     return post
+
 
 def md5(md5str):
     import hashlib
@@ -161,17 +205,21 @@ def md5(md5str):
     m.update(md5str)
     return m.hexdigest()
 
+
 def uc_api_input(data):
-    return urllib.quote_plus(uc_authcode(data + "&agent=" + \
-                                       md5("Python-urllib/2.7") + \
-                             "&time=" + str(int(time.time())), \
-                             "ENCODE", UC_KEY))
+    return urllib.quote_plus(uc_authcode(data + "&agent=" +
+                                         md5("Python-urllib/2.7") +
+                                         "&time=" + str(int(time.time())),
+                                         "ENCODE", UC_KEY))
+
 
 def microtime(get_as_float=False):
     if get_as_float:
         return time.time()
     else:
         return '%f %d' % math.modf(time.time())
+
+
 def substr(string, start, length=None):
     """Returns the portion of string specified by the start and length
     parameters.
@@ -192,8 +240,11 @@ def base64_decode(data):
     if missing_padding:
         data += b'=' * missing_padding
     return data.decode('base64')
+
+
 def base64_encode(data):
     return data.encode('base64')
+
 
 def uc_authcode(string, operation='DECODE', key='', expiry=0):
     ckey_length = 4
@@ -201,13 +252,13 @@ def uc_authcode(string, operation='DECODE', key='', expiry=0):
     keya = md5(substr(key, 0, 16))
     keyb = md5(substr(key, 16, 16))
     keyc = substr(string, 0, ckey_length) if operation == 'DECODE' \
-                      else substr(md5(microtime()), -ckey_length)
+        else substr(md5(microtime()), -ckey_length)
     cryptkey = keya + md5(keya + keyc)
     key_length = len(cryptkey)
     string = base64_decode(substr(string, ckey_length)) if \
-             operation == 'DECODE' else  (\
-                '%010d' % (expiry + int(time.time()) if expiry else 0)) \
-             + substr(md5(string + keyb), 0, 16) + string
+        operation == 'DECODE' else \
+        ('%010d' % (expiry + int(time.time()) if expiry else 0)) \
+        + substr(md5(string + keyb), 0, 16) + string
     string_length = len(string)
 
     result = ''
@@ -233,21 +284,23 @@ def uc_authcode(string, operation='DECODE', key='', expiry=0):
                               (box[(box[a] + box[j]) % 256]))
     if operation == 'DECODE':
         if (int(substr(result, 0, 10)) == 0 or \
-                 int(substr(result, 0, 10)) - \
-                 int(time.time()) > 0) and \
-                 substr(result, 10, 16) == \
-                 substr(md5(substr(result, 26) + keyb), 0, 16):
+                            int(substr(result, 0, 10)) - \
+                                int(time.time()) > 0) and \
+                        substr(result, 10, 16) == \
+                        substr(md5(substr(result, 26) + keyb), 0, 16):
             return substr(result, 26)
         else:
             return ''
     else:
         return keyc + base64_encode(result).replace('=', '')
 
+
 def test():
     res = uc_user_login("admin", "admin")
     print res
     res = uc_user_logincheck('admin', '127.0.0.1')
     print res
+
+
 if __name__ == "__main__":
     test()
-
